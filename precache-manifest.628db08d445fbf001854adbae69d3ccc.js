af137ddd7e6db52babc4345de8cad0f33bd74f23
blabbylobster60 <marcfalquez@gmail.com>
2019-04-21 13:11:51 -0400

    Add files via upload

---
+++ precache-manifest.628db08d445fbf001854adbae69d3ccc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/C/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/C/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/C/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/C/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/C/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "1e7163441e681101363f",
+    "url": "/C/static/js/runtime~main.1e716344.js"
+  },
+  {
+    "revision": "814d66fb5f8ccb1d7f45",
+    "url": "/C/static/js/main.814d66fb.chunk.js"
+  },
+  {
+    "revision": "30e578a2596d39c34d71",
+    "url": "/C/static/js/1.30e578a2.chunk.js"
+  },
+  {
+    "revision": "814d66fb5f8ccb1d7f45",
+    "url": "/C/static/css/main.726bea90.chunk.css"
+  },
+  {
+    "revision": "bcf6d501afec33cf43c46119bc5c8dcb",
+    "url": "/C/index.html"
+  }
+];
